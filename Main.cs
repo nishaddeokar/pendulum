b78797a64e04eb0384a4071082e6591a2ca1fe5c
nishaddeokar <116658820+nishaddeokar@users.noreply.github.com>
2022-10-27 10:43:38 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -8,8 +8,8 @@ public class Main : MonoBehaviour
     public Transform pivot;
     public Transform node;
     public Transform mass;
-    public Slider slider1; //already a variable named slider in Slider_length.cs. Therefore, named slider1 to avoid confusion.
-
+    public Slider slider1; // already a variable named slider in Slider_length.cs. 
+                           // Therefore, named slider1 to avoid confusion.
     public float maxDisplacement;
     public float currentDisplacement;
     public float lengthOfRod = 1f; //arbitrary value
@@ -23,9 +23,12 @@ public class Main : MonoBehaviour
 
     float FindLength(Transform mass, Transform pivot)
     {
-        float length = Mathf.Sqrt(((mass.transform.position.x - pivot.transform.position.x) * (mass.transform.position.x - pivot.transform.position.x))
-            + ((mass.transform.position.y - pivot.transform.position.y) * (mass.transform.position.y - pivot.transform.position.y))
-            + ((mass.transform.position.z - pivot.transform.position.z) * (mass.transform.position.z - pivot.transform.position.z)));
+        float length = Mathf.Sqrt(((mass.transform.position.x - pivot.transform.position.x) 
+                                   * (mass.transform.position.x - pivot.transform.position.x))
+                                  + ((mass.transform.position.y - pivot.transform.position.y) 
+                                     * (mass.transform.position.y - pivot.transform.position.y))
+                                    + ((mass.transform.position.z - pivot.transform.position.z) 
+                                       * (mass.transform.position.z - pivot.transform.position.z)));
         return length;
     }
 
@@ -33,14 +36,16 @@ public class Main : MonoBehaviour
     {
         lengthOfRod = (FindLength(mass, pivot) - 5) / 10;
         maxDisplacement = maxAngle * lengthOfRod;
-        series = (1 + (0.0625f * Mathf.Pow(maxAngle, 2)) + (0.0035807291667f * Mathf.Pow(maxAngle, 4)) + (0.00023464626736f * Mathf.Pow(maxAngle, 6)) + (0.000017356115674f * Mathf.Pow(maxAngle, 8)) + (0.0000013867625063f * Mathf.Pow(maxAngle, 10)));
+        series = 1 + (0.0625f * Mathf.Pow(maxAngle, 2)) + (0.0035807291667f * Mathf.Pow(maxAngle, 4)) 
+                 + (0.00023464626736f * Mathf.Pow(maxAngle, 6)) + (0.000017356115674f * Mathf.Pow(maxAngle, 8)) 
+                 + (0.0000013867625063f * Mathf.Pow(maxAngle, 10));
         timePeriod = (2 * Mathf.PI) * Mathf.Sqrt(lengthOfRod / gravitationalAcceleration) *  series;
     }
 
     void SetNodePos()
     {
         node.gameObject.SetActive(true);
-        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y); //the z-coordinate does not need to be defined, as it is defined separately in the subsequent line.
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y); //the z-coordinate is defined separately in the subsequent line.
         mouse.z = 20f; //this is set to 20 as this is the distance the pivot (and therefore the mass) is from the camera.
         node.transform.position = Camera.main.ScreenToWorldPoint(mouse);
     }
@@ -57,12 +62,14 @@ public class Main : MonoBehaviour
     void IfClicked()
     {
         SetNodePos();
-        maxAngle = Mathf.Atan2((node.transform.position.y - pivot.transform.position.y), (node.transform.position.x - pivot.transform.position.x)) + (0.5f * Mathf.PI); //resultant angle is in radians.
+        maxAngle = Mathf.Atan2((node.transform.position.y - pivot.transform.position.y), 
+                               (node.transform.position.x - pivot.transform.position.x)) 
+                   + (0.5f * Mathf.PI); //resultant angle is in radians.
         if (maxAngle > Mathf.PI) 
         { 
             maxAngle = maxAngle - (2 * Mathf.PI); 
         }
-        pivot.transform.localEulerAngles = new Vector3(0, 0, maxAngle * Mathf.Rad2Deg); //Euler angles are in degrees. Therefore, maxAngle must be converted from radians to degrees.
+        pivot.transform.localEulerAngles = new Vector3(0, 0, maxAngle * Mathf.Rad2Deg); //Euler angles are in degrees. 
         currentTime = 0;
     }
 
@@ -71,7 +78,7 @@ public class Main : MonoBehaviour
         CalculateConstants();
         currentDisplacement = maxDisplacement * Mathf.Cos((2 * Mathf.PI / timePeriod) * currentTime);
         currentAngle = currentDisplacement / lengthOfRod; //this angle is in radians.
-        pivot.transform.localEulerAngles = new Vector3(0, 0, currentAngle * Mathf.Rad2Deg); //Euler angles are in degrees. Therefore, currentAngle must be converted from radians to degrees.
+        pivot.transform.localEulerAngles = new Vector3(0, 0, currentAngle * Mathf.Rad2Deg); //Euler angles are in degrees.
     }
 
     void Damping()
